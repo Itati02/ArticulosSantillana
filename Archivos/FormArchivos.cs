465db47d23c1ce91a3ee2c00c8ae383149109c90
Itati02 <89601723+Itati02@users.noreply.github.com>
2021-11-03 23:07:51 -0600

    Add files via upload

---
+++ Archivos/FormArchivos.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Archivos
+{
+    public partial class FormArchivos : Form
+    {
+        articulos[] arregloart;
+        int art = 0;
+        private object lstArreglos;
+       
+        public FormArchivos()
+        {
+            InitializeComponent();
+            arregloart = new articulos[4];
+        }
+       
+        private void BtnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogo = new OpenFileDialog();
+            if(dialogo.ShowDialog()== DialogResult.OK)
+            {
+                StreamReader Sr = new StreamReader(dialogo.FileName);
+            }
+        }
+        private void BtnGuardar_click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogo = new SaveFileDialog();
+            if (dialogo.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter articulos = new StreamWriter(dialogo.FileName))
+                {
+                    for (int i = 0; i < arregloart.Length; i++)
+                    {
+                        if (arregloart[i] != null)
+                        {
+                            articulos.WriteAsync(arregloart[i].ToString());
+                        }
+                    }
+                }
+                
+
+            }
+        }
+        private void FormArchivos_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void BtnInsertar_Click(object sender, EventArgs e)
+        {
+            articulos a = new articulos();
+            a.Nombre=TxtNombre.Text;
+            a.Precio=double.Parse(TxtPrecio.Text);
+            arregloart[art++] = a;
+
+            lstArreglos.Items.Add(arregloart[art]);
+            art++;
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ Archivos/articulos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    class articulos
+    {
+        private string nombre;
+        private double precio;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+
+        }
+
+        public double Precio
+        {
+            get
+            {
+                return precio;
+            }
+            set
+            {
+                precio = value;
+            }
+        }
+
+        public articulos()
+        {
+            this.nombre = "";
+            this.Precio = 0;
+        }
+        public articulos(string nombre, double precio)
+        {
+            this.nombre = nombre;
+            Precio = precio;
+        }
+        public override string ToString()
+        {
+            return nombre+""+ Precio+"";
+        }
+    }
+}
